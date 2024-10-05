22bdfa15397535afc4afa7586ab84bd6b919ba9c
Henrique Veronez <v3ronez.dev@gmail.com>
2024-10-05 15:18:33 -0300

    fix bufferline

--- lua/custom/plugins/bufferline.lua
+++ lua/custom/plugins/bufferline.lua
@@ -3,12 +3,14 @@ return {
   version = '*',
   dependencies = 'nvim-tree/nvim-web-devicons',
   opts = {
-    offsets = {
-      {
-        filetype = 'neo-tree',
-        text = 'Neo-tree',
-        highlight = 'Directory',
-        text_align = 'left',
+    options = {
+      offsets = {
+        {
+          filetype = 'neo-tree',
+          text = 'Neo-tree',
+          highlight = 'Directory',
+          text_align = 'left',
+        },
       },
     },
   },
