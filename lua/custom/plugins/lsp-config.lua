aac802a648c7368167b725f036ccdd17e26c6078
Henrique Veronez <v3ronez.dev@gmail.com>
2025-01-08 21:33:08 -0300

    new

--- lua/custom/plugins/lsp-config.lua
+++ lua/custom/plugins/lsp-config.lua
@@ -171,7 +171,23 @@ return {
       -- clangd = {},
       -- gopls = {},
       -- pyright = {},
-      -- rust_analyzer = {},
+      rust_analyzer = {
+        flags = {
+          debounce_text_changes = 150,
+        },
+        capabilities = capabilities,
+        settings = {
+          ['rust_analyzer'] = {
+            completion = {
+              postfix = { enable = true },
+            },
+          },
+          cargo = {
+            features = 'all',
+            allFeatures = true,
+          },
+        },
+      },
       -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
       --
       -- Some languages (like typescript) have entire language plugins that can be useful:
