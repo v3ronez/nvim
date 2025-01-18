d37e0df72c6df2ca682955a863fefda1053b5dae
Henrique Veronez <v3ronez.dev@gmail.com>
2025-01-18 14:37:58 -0300

    remove default statusLine kickstarter and config new status line to rose pine

--- lua/custom/plugins/rose-pine.lua
+++ lua/custom/plugins/rose-pine.lua
@@ -1,4 +1,16 @@
--- lua/plugins/rose-pine.lua
+vim.opt.laststatus = 3 -- Or 3 for global statusline
+-- vim.opt.statusline = '   %f %m %= %l:%c λ    '
+
+-- Function to get the current Git branch
+local function get_git_branch()
+  local branch = vim.fn.system('git branch --show-current 2>/dev/null'):gsub('\n', '')
+  return branch ~= '' and branch or 'no-branch'
+end
+
+_G.get_git_branch = get_git_branch
+
+-- Set the status line
+vim.opt.statusline = '   %f %m %= %l:%c [%{v:lua.get_git_branch()}] λ    '
 return {
   'rose-pine/neovim',
   name = 'rose-pine',
@@ -11,6 +23,20 @@ return {
         base = '#000000', -- Define o fundo como preto
         overlay = '#000000',
         surface = '#000000',
+      },
+      highlight_groups = {
+        --statusLine
+        StatusLine = { fg = '#f1f1f1', bg = 'base', blend = 10 },
+        StatusLineNC = { fg = 'subtle', bg = 'surface' },
+        --statudLine end
+        --telescope
+        TelescopeBorder = { fg = 'highlight_high', bg = 'none' },
+        TelescopeNormal = { bg = 'none' },
+        TelescopePromptNormal = { bg = 'base' },
+        TelescopeResultsNormal = { fg = 'subtle', bg = 'none' },
+        TelescopeSelection = { fg = 'text', bg = 'base' },
+        TelescopeSelectionCaret = { fg = 'iris', bg = 'iris' },
+        --telescope end
       },
     }
     vim.cmd 'colorscheme rose-pine-moon'
