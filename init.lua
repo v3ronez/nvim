607115e5bbd27aeb692505d56cba85356ebf125c
Henrique Veronez <v3ronez.dev@gmail.com>
2025-01-23 14:01:26 -0300

    add again telescope ui

--- init.lua
+++ init.lua
@@ -264,7 +264,7 @@ require('lazy').setup({
       },
 
       -- Useful for getting pretty icons, but requires a Nerd Font.
-      -- { 'nvim-telescope/telescope-ui-select.nvim' },
+      { 'nvim-telescope/telescope-ui-select.nvim' },
       -- { 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font },
       -- { 'echasnovski/mini.icons', opts = {} } --better icons,
     },
