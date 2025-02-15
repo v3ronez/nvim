0c55bef6fb0c7026dd5498915d32b90f0844a8a9
Henrique Veronez <v3ronez.dev@gmail.com>
2025-02-14 22:35:31 -0300

    new buffer navigation

--- init.lua
+++ init.lua
@@ -112,6 +112,9 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 --
+-- BUFFER NAVIGATION
+vim.keymap.set('n', '<C-l>', '<cmd>bnext<cr>', { desc = 'Next Buffer' })
+vim.keymap.set('n', '<C-h>', '<cmd>bprevious<cr>', { desc = 'Prev Buffer' })
 -- OLD NAVIGATION
 -- vim.keymap.set('n', '<C-]>', '<cmd>bnext<cr>', { desc = 'Next Buffer' })
 -- vim.keymap.set('n', '<C-[>', '<cmd>bprevious<cr>', { desc = 'Prev Buffer' })
