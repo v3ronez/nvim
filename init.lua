daa467cebec926b39c0cc61d93c716bd13686565
v3ronez <v3ronez.dev@gmail.com>
2025-04-09 14:31:44 -0300

    [b to switch by buffers

--- init.lua
+++ init.lua
@@ -143,10 +143,6 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 --
--- BUFFER NAVIGATION
-vim.keymap.set('n', '<leader>n', '<cmd>bnext<CR>', { desc = 'Next Buffer', noremap = true, silent = true })
-vim.keymap.set('n', '<leader>b', '<cmd>bprevious<CR>', { desc = 'Prev Buffer', noremap = true, silent = true })
---
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
